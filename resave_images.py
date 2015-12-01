657f44ff85bc8ba711ca752a769cbde0de3bd76f
monotonee <monotonee@tuta.io>
2015-12-01 16:40:51 -0500

    Added a crude script to iteratively resave image files under a given directory.

---
+++ resave_images.py
@@ -0,0 +1,31 @@
+# This file, when executed and supplied a path to a target directory, will open
+# each JPG and PNG image and resave each in PNG format under a new name. As most
+# training and test images were pulled from random hosts on the Internet, the
+# operations performed by this script normalize file names and in theory
+# sanitize the image files of any attempt at malicious code inclusion,
+# correcting color profile errors in the process. PNG format is uniformally used
+# to avoid the degredation of image quality over repeated JPG compression on an
+# image. The original image files are deleted after the resave operations.
+# @param directoryPath {string} The directory in which the image files exist.
+# @warning The original image files are deleted.
+
+import os
+from PIL import Image
+import sys
+
+directoryPath = os.path.realpath(sys.argv[1])
+
+iterationCount = 0
+for srcFileName in os.listdir(directoryPath):
+  srcFilePath = os.path.join(directoryPath, srcFileName)
+  srcFileNameSplit = srcFileName.split('.')
+  srcFileExtension = srcFileNameSplit[len(srcFileNameSplit) - 1]
+  if srcFileExtension == 'jpg' or srcFileExtension == 'png':
+    image = Image.open(srcFilePath)
+    destFileName = str(iterationCount).zfill(3) + '.png'
+    image.save(os.path.join(directoryPath, destFileName))
+    os.remove(srcFilePath)
+    iterationCount += 1
+  else:
+    print('failed: ' + srcFilePath)
+
