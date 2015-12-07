6bb6552d5da424681a51b16921dae50173269624
monotonee <monotonee@tuta.io>
2015-12-07 14:48:03 -0500

    Added script parameter to specify path to OpenCV cascade file.

--- test/detect_nfpa704.py
+++ test/detect_nfpa704.py
@@ -2,6 +2,8 @@
 # this will run the cascade against the image and display a temporary image
 # with the detections outlined in blue rectangles. The image will display for
 # ten seconds or until any key is pressed.
+#
+# @param cascadePath {string} The path to the OpenCV cascade file (XML).
 # @param testImagePath {string} The path to the image on which detection should
 #   should be run.
 
@@ -9,9 +11,10 @@ import cv2 as cv
 import os
 import sys
 
-testImagePath = os.path.realpath(sys.argv[1])
+cascadePath = os.path.realpath(sys.argv[1])
+testImagePath = os.path.realpath(sys.argv[2])
 
-cascade = cv.CascadeClassifier('../images/cascade/48x5000/cascade.xml')
+cascade = cv.CascadeClassifier(cascadePath)
 image = cv.imread(testImagePath)
 greyscaleImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 objects = cascade.detectMultiScale(greyscaleImage)
